ab68019a10ab27bdcc5abe0162686d75e69f4df7
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-03-28 18:53:38 +0000

    succesfullyの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user.id)
+    redirect_to user_path(@user.id),notice: "You have updated user successfully."
   end
   def index
     @user = User.find(current_user.id)
