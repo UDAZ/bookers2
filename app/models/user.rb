4cd7009724b4f1a02fea23e866a97590f7152be1
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-03-28 18:12:04 +0000

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   attachment :profile_image
   has_many :books, dependent: :destroy
+  validates :name, presence: true, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
 end
