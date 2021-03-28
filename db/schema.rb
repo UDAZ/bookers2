6f1ae7e5cfa79af7c44279b932bd9c183a60989c
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-03-28 14:11:14 +0000

    book model opinion > body

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_165324) do
+ActiveRecord::Schema.define(version: 2021_03_28_140119) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.text "opinion"
+    t.text "body"
     t.string "image_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
