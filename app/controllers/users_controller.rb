4a5c2c5417ef768938b51600ce9e3b3ebe0ec53f
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-03-28 20:47:48 +0000

    rspec実施後修正-画像表示-リダイレクト-user編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+before_action :ensure_correct_user, only:[:edit]
   def edit
     @user = User.find(params[:id])
   end
@@ -29,4 +29,10 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
+    def ensure_correct_user
+    @user = User.find(params[:id])
+     unless @user.id == current_user
+     redirect_to user_path(current_user.id)
+     end
+    end
 end
